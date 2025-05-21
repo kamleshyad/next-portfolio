d1f013d3d6e20826cba96dd6082b259c374a0b12
kamleshyad <kamleshyadav1795@gmail.com>
2025-05-21 12:43:55 +0530

    about section updated

--- app/_components/organisms/aboutsec/Aboutsec.jsx
+++ app/_components/organisms/aboutsec/Aboutsec.jsx
@@ -1,46 +1,38 @@
 import { PrimaryLink } from '../../atoms/buttons/Button';
-import { Sectiontitle } from '../../molecules/sectiontitle/Sectiontitle';
 import styles from './page.module.scss';
 
 export const Aboutsec = () => {
+
+    const aboutItems = [
+        { label: "Name", value: "Kamlesh S. Yadav" },
+        { label: "Work Profile", value: "Frontend Developer" },
+        { label: "Birthday", value: "February 19, 1995" },
+        { label: "Phone", value: "+91 9168737879", type: "phone" },
+        { label: "Email", value: "Kamleshyadav1795@gmail.com", type: "email" },
+        { label: "Location", value: "Palghar, Maharashtra" },
+        { label: "Languages", value: "English, Hindi, Marathi" },
+    ]
+
     return (
         <article className={styles.aboutwrap}>
             <div className={styles.aboutlefttsec}>
                 <ul className="reset">
-                    <li>
-                        <p className={styles.title}>Name</p>
-                        <p>Kamlesh S. Yadav</p>
-                    </li>
-                    <li>
-                        <p className={styles.title}>Work Profile</p>
-                        <p>Frontend Developer</p>
-                    </li>
-                    <li>
-                        <p className={styles.title}>Birthday</p>
-                        <p>February 19, 1995</p>
-                    </li>
-                    <li>
-                        <p className={styles.title}>Phone</p>
-                        <p>+91 9168737879</p>
-                    </li>
-                    <li>
-                        <p className={styles.title}>Email</p>
-                        <p>Kamleshyadav1795@gmail.com</p>
-                    </li>
-                    <li>
-                        <p className={styles.title}>Location</p>
-                        <p>Palghar, Maharashtra</p>
-                    </li>
-                    <li>
-                        <p className={styles.title}>Language</p>
-                        <p>English, Hindi, Marathi</p>
-                    </li>
+                    {
+                        aboutItems.map((about, index) => {
+                            return(
+                                <li key={index}>
+                                    <p className={styles.title}>{about.label}</p>
+                                    <p>{about.value}</p>
+                                </li>
+                            )
+                        })
+                    }
                 </ul>
             </div>
             <div className={styles.aboutrightsec}>
-                <h4>More about me</h4>
+                <h3>More about me</h3>
                 <p>Front-end developer with 3.6 of experience in creating dynamic, responsive, and user-focused web applications using HTML5, CSS3, JavaScript, Wordpress, React and Next.js. Skilled in integrating APIs, optimizing for SEO, and enhancing performance to improve user engagement. Dedicated to delivering clean, scalable code and staying updated with the latest industry trends.</p>
-                <PrimaryLink href="/files/Kamlesh-S-Yadav-Resume.pdf" aria-label="Link to downliad cv" download>Download CV</PrimaryLink>
+                <PrimaryLink href="/doc/Kamlesh-S-Yadav-Resume.pdf" aria-label="Download Kamlesh Yadav's resume as PDF" download>Download CV</PrimaryLink>
             </div>
         </article>
     )
