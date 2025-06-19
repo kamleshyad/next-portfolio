3646eb42acb4febefa9213e4371358acd348f71d
kamleshyad <kamleshyadav1795@gmail.com>
2025-06-19 12:57:31 +0530

    footer section added

--- app/_components/organisms/footer/Footer.jsx
+++ app/_components/organisms/footer/Footer.jsx
@@ -1,11 +1,71 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
 import styles from './footer.module.scss';
+
+import contactImg from '../../../../public/images/contact-img.png';
+import { PhoneIcon } from '../../atoms/icons/Icons';
+import { LocationIcon } from '../../atoms/icons/Icons';
+import { MailIcon } from '../../atoms/icons/Icons';
 import { Container } from "../../template/container/Container";
+import { Sectiontitle } from '../../molecules/sectiontitle/Sectiontitle';
 
 export const Footer = () => {
+
+    const pathName = usePathname();
+
     return (
         <footer className={styles.sitefooter}>
             <Container container="lg-">
-                <h2>Footer</h2>
+                {
+                    pathName == "/" ? <Sectiontitle id="Contact" title="Get In Touch"/> : ""
+                }
+                
+                <div className={styles.contactsec}>
+                    <div className={styles.contactleft}>
+                        <figure>
+                            <Image src={contactImg} alt="footer contact img"/>
+                        </figure>
+                    </div>
+                    <div className={styles.contactright}>
+                        {
+                            pathName == "/" ? <h3>Let’s Talk</h3> : <h3>Get In Touch</h3>
+                        }
+                        <p>Let's make something great together. We are trusted by over 5000+ clients. Join them by using our services and grow your business.</p>
+                        <div className={styles.contactbox}>
+                            <div className={styles.contactlist}>
+                                <figure className={styles.icon}>
+                                    <LocationIcon />
+                                </figure>
+                                <div className={styles.info}>
+                                    <h4>Address</h4>
+                                    <p>Mumbai, Maharashtra</p>
+                                </div>
+                            </div>
+                            <div className={styles.contactlist}>
+                                <figure className={styles.icon}>
+                                    <PhoneIcon />
+                                </figure>
+                                <div className={styles.info}>
+                                    <h4>Phone</h4>
+                                    <Link href="tel:9168737879">9168737879</Link>
+                                </div>
+                            </div>
+                            <div className={styles.contactlist}>
+                                <figure className={styles.icon}>
+                                    <MailIcon />
+                                </figure>
+                                <div className={styles.info}>
+                                    <h4>Email</h4>
+                                    <Link href="mailto:kamleshyadav1795@gmail.com">kamleshyadav1795@gmail.com</Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </Container>
         </footer>
     )
