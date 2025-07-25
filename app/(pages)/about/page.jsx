c4caed2e2aab740c3b8ba97c8cad1634bce64288
kamleshyad <kamleshyadav1795@gmail.com>
2025-07-25 16:49:40 +0530

    text gradient and background added

--- app/(pages)/about/page.jsx
+++ app/(pages)/about/page.jsx
@@ -5,14 +5,8 @@ import { Aboutsec } from "@/app/_components/organisms/aboutsec/Aboutsec";
 
 export default function About() {
     return (
-        <section className={styles.aboutsec} aria-labelledby='about-me'>
-            <MainTemplate container="lg-">
-                <Aboutsec />
-                <Aboutsec />
-                <Aboutsec />
-                <Aboutsec />
-                <Aboutsec />
-            </MainTemplate>
-        </section>
+        <div className="site__content">
+            
+        </div>
     )
 }
